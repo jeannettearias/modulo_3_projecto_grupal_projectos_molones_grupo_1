7cdf20ed8bd74477300105882df4d827a09e6cf5
aruiz982 <aruiz@elconfidencial.com>
2024-08-27 19:57:16 +0200

    Form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -3,7 +3,6 @@ import ButtonAuthorPicture from "./ButtonAuthorPicture";
 import ButtonProjectPicture from "./ButtonProjectPicture";
 import ButtonSaveProject from "./ButtonSaveProject";
 
-<<<<<<< HEAD
 function Form() {
 
   const [allValues, setAllValues] = useState({
@@ -103,81 +102,6 @@ function Form() {
           onChange={getInputValue}
         />
       </fieldset>
-=======
-import PropTypes from 'prop-types';
-
-function Form({ uploadProjectPicture, handleUploadPicture }) {
-  return (
-    <>
-      <form className="addForm">
-        <h2 className="title">Información</h2>
-        <fieldset className="addForm__group">
-          <legend className="addForm__title">Cuéntanos sobre el proyecto</legend>
-          <input
-            className="addForm__input"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Nombre del proyecto"
-          />
-          <input
-            className="addForm__input"
-            type="text"
-            name="slogan"
-            id="slogan"
-            placeholder="Slogan"
-          />
-          <div className="addForm__2col">
-            <input
-              className="addForm__input"
-              type="url"
-              name="repo"
-              id="repo"
-              placeholder="Repositorio"
-            />
-            <input
-              className="addForm__input"
-              type="url"
-              name="demo"
-              id="demo"
-              placeholder="Demo"
-            />
-          </div>
-          <input
-            className="addForm__input"
-            type="text"
-            name="technologies"
-            id="technologies"
-            placeholder="Tecnologías"
-          />
-          <textarea
-            className="addForm__input"
-            type="text"
-            name="desc"
-            id="desc"
-            placeholder="Descripción"
-            rows="5"
-          ></textarea>
-        </fieldset>
-
-        <fieldset className="addForm__group">
-          <legend className="addForm__title">Cuéntanos sobre la autora</legend>
-          <input
-            className="addForm__input"
-            type="text"
-            name="autor"
-            id="autor"
-            placeholder="Nombre"
-          />
-          <input
-            className="addForm__input"
-            type="text"
-            name="job"
-            id="job"
-            placeholder="Trabajo"
-          />
-        </fieldset>
->>>>>>> 1bb658478d8d1ff49e9469c7c0056233c93c1cf3
 
         <fieldset className="addForm__group--upload">
           <ButtonProjectPicture
